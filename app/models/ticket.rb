b9d19a6ff1491bcdcb4101b81510370001cd6903
leizzer <lizzydw@gmail.com>
2009-12-28 08:43:52 -0300

    Validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,5 @@
 class Ticket < ActiveRecord::Base
+  validates_presence_of :tickettype_id
+  validates_numericality_of :tickettype_id
+  validates_presence_of :owner
 end
--- app/models/tickettype.rb
+++ app/models/tickettype.rb
@@ -1,2 +1,6 @@
 class Tickettype < ActiveRecord::Base
+  validates_presence_of :amount
+  validates_numericality_of :amount
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
