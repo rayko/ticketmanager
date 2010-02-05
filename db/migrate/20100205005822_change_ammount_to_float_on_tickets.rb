dd0c83ecab1159460db8f3180f0e5886bce8931b
rayko <rayko@rayko-server.(none)>
2010-02-04 23:55:10 -0300

    Corrected a mistake

--- db/migrate/20100205005822_change_ammount_to_float_on_tickets.rb
+++ db/migrate/20100205005822_change_ammount_to_float_on_tickets.rb
@@ -3,13 +3,13 @@ class ChangeAmmountToFloatOnTickets < ActiveRecord::Migration
     change_column :tickettypes, :amount, :float, :default => 0
     change_column :tickettypes, :name, :string, :default => ''
     change_column :tickets, :paid, :boolean, :default => false
-    change_column :tickets, :owner, :boolean, :default => ''
+    change_column :tickets, :owner, :string, :default => ''
   end
 
   def self.down
     change_column :tickettypes, :amount, :string
     change_column :tickettypes, :name, :string
     change_column :tickets, :paid, :boolean
-    change_column :tickets, :owner, :boolean
+    change_column :tickets, :owner, :string
   end
 end
