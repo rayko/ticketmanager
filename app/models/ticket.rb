bc45025d08c6afbaf96bc2531067ea9bc8025132
rayko <rayko@raykosite.com.ar>
2010-03-01 09:57:44 -0300

    Clean app

--- app/models/ticket.rb
+++
@@ -1,5 +0,0 @@
-class Ticket < ActiveRecord::Base
-  validates_presence_of :tickettype_id
-  validates_numericality_of :tickettype_id
-  validates_presence_of :owner
-end
--- db/migrate/20091218112528_add_amount_to_tickettype.rb
+++
@@ -1,9 +0,0 @@
-class AddAmountToTickettype < ActiveRecord::Migration
-  def self.up
-    add_column :tickettypes, :amount, :string
-  end
-
-  def self.down
-    remove_column :tickettypes, :amount
-  end
-end
